3273dd3ab90d218fee6da5578d5063ab9d628a8d
Muhammad Ma'dum <muhammad.madum2018@gmail.com>
2022-03-10 16:45:23 +0700

    selesai

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    $('#example').DataTable();
+    // $('#example').DataTable();
+    $('.testselect3').SumoSelect({placeholder: 'None Selected'});
 } );
+
+$(".box-custom-select").toggle();
+
+var select = $("[data-toggle=select]");
+var option = $("[data-toggle=option]");
+
+for (let i = 0; i < select.length; i++) {
+    select[i].onclick = function () {
+        var id_target = $(this).data('target');
+        $(id_target).toggle();
+    }
+}
+
